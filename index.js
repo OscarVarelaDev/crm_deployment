d2a5b51549bc27166d08a7285dd5bc179a10bb36
Oscar <64058694+OscarVarelaDev@users.noreply.github.com>
2023-04-18 23:56:25 -0600

    cambios de variables de entorno

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const routes = require('./routes')
 const mongoose = require('mongoose');
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const cors = require('cors')
-require('dotenv').config({path: 'variables.env'})
+require('dotenv').config({ path: 'variables.env' })
 //Cors permite el intercambio de recursos
 
 
 //conectar mongodb
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_URL , {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
-  
+
 })
 
 const app = express();
+app.use(express.static('uploads'))
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const whitelist = [process.env.FRONTEND_URL]
 const corsOptions = {
@@ -35,16 +36,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 //carpeta publica
-app.use(express.static('uploads'))
+
 
 app.use('/', routes())
 
 
-const host = process.env.HOST ||  '0.0.0.0'
-const port = process.env.PORT || 5173
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 4000
 
 
-app.listen(port,host, () => {
+app.listen(port, host, () => {
     console.log('El servidor esta funcionando')
 }
 )
